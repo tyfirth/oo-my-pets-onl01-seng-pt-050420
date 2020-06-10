f25e3edf1a8efb7523cb9e477dbc74b0b754bdda
Tyler Firth <tyfirth@gmail.com>
2020-06-10 21:47:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,61 @@
 class Owner
-  # code goes here
+ 
+attr_reader :name, :species
+attr_accessor :pets, :dog, :cat, :dogs, :cats
+ 
+  @@all = []
+  @@pets = {:cats => [], :dogs => []}
+ 
+def initialize(name)
+  @name = name
+  @species = "human"
+  @@all << self
+end
+ 
+def say_species
+  "I am a #{@species}."
+end
+ 
+def self.all
+  @@all
+end
+ 
+def self.count
+  @@all.count
+end
+ 
+def self.reset_all
+  @@all.clear
+end
+ 
+def buy_cat(name, owner)
+    name = Cat.new(name, self)
+    @@pets[:cats] << name 
+end
+ 
+def buy_dog(name, owner)
+  @@pets[:dogs] << Dog.new(name, self)
+end
+ 
+def cats 
+  @@pets[:cats]
+end
+ 
+def dogs
+  @@pets[:dogs]
+end
+ 
+  def list_pets
+    @@pets
+  end
+  
+  def sell_pets
+    pets.each do |pet, arr|
+      arr.map do |pet|
+        pet.mood = 'nervous'
+      end
+      arr.clear
+    end
+  end
+ 
 end
